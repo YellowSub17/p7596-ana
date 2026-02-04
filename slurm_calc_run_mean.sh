f45ed489e175d16e50bf66d90cb235943d299d5e
Patrick Adams <patrick_aust@hotmail.com>
2026-02-04 23:19:48 +1100

    s

--- slurm_calc_run_mean.sh
+++ slurm_calc_run_mean.sh
@@ -8,9 +8,13 @@ unset LD_PRELOAD
 source /etc/profile.d/modules.sh
 
 module purge
-
 module load exfel exfel-python openmpi-no-python
 
+echo $(date)
+
+echo "$@"
 
 mpirun -n 32 -- python calc_run_mean.py $@
+
+echo $(date)
 
